cb7396f21aa6aa33c5ff8d8519d21febc827c5f5
Chinmay Hegde <62785622+Dacrron@users.noreply.github.com>
2024-07-15 14:49:35 +0530

    Update config.js
    Removed Personal Invokes ,api and userpools

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_Dv2dVmqH6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a7kh62qs1hi5vqmpnepkc11b9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpbndpcnmk.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
